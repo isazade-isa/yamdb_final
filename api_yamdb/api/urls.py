dadcc8c4a123da752554573ee1417940835b4839
Исазаде Иса <isazade-isa@yandex.com>
2022-09-18 14:57:14 +0300

    sp16-001-12

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,9 @@
 from api.views import (
-    APIGetToken, APIGetConfirmationCode, CategoryViewSet, CommentsViewSet,
+    APIGetConfirmationCode, APIGetToken, CategoryViewSet, CommentsViewSet,
     GenreViewSet, ReviewsViewSet, TitleViewSet, UserViewSet
 )
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-
 
 app_name = 'api'
 
